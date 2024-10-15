12ed26dc422ed55ef95dd5e6873a6be827c75e42
aadith2 <aadithraj000@gmail.com>
2024-10-15 21:28:31 +0530

    fixed the sidebar

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -6,7 +6,7 @@
 </div>
 
 <hr style="border:1px solid; background-color:#8a7b6d; border-color:#3B3131;">
-    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+    <!-- <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a> -->
     <a href="./index.php" ><i class="fa fa-home"></i> Dashboard</a>
     <a href="#players"  onclick="showPlayers()" ><i class="fa fa-users"></i> Players</a>
     <!-- <a href="#"   onclick="showCategory()" ><i class="fa fa-th-large"></i> Category</a> -->
