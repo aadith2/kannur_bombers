580623e9a70a8d10c7452a660328331d7212816f
aadith2 <aadithraj000@gmail.com>
2024-10-15 21:18:31 +0530

    Added code to delete player image when a player record is deleted

--- admin/controller/deletePlayerController.php
+++ admin/controller/deletePlayerController.php
@@ -5,6 +5,16 @@
     $player_id=$_POST['record'];
     $query="DELETE FROM players where player_id='$player_id'";
 
+    $img_result = mysqli_query($conn, "SELECT image FROM players WHERE player_id='$player_id'");
+
+    if ($img_result) {
+        // Fetch the row as an associative array
+        $row = mysqli_fetch_assoc($img_result);
+        
+        // Get the image string from the row
+        $image = '.'.$row['image'];
+        unlink($image);
+    }
     $data=mysqli_query($conn,$query);
 
     if($data){
