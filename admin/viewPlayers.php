8f6b22fe5c9e967bcb1bab84184046af8ee1b069
aadith2 <aadithraj000@gmail.com>
2024-10-15 21:20:45 +0530

    Updated add and close btn colors

--- admin/viewPlayers.php
+++ admin/viewPlayers.php
@@ -79,13 +79,13 @@
                 <input type="file" class="form-control-file" id="image">
             </div>
             <div class="form-group">
-              <button type="submit" class="btn btn-secondary" id="upload" style="height:40px">Add player</button>
+              <button type="submit" class="btn btn-success" id="upload" style="height:40px">Add player</button>
             </div>
           </form>
 
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal" style="height:40px">Close</button>
+          <button type="button" class="btn btn-danger" data-dismiss="modal" style="height:40px">Close</button>
         </div>
       </div>
       
